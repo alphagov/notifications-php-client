6d5680c76cc0f9100a9651bc6ca30aef5cd62a62
Alex Saunders <acsauk@gmail.com>
2022-04-06 16:16:22 +0100

    Bump to 4.0.0 due to PHP minimum version requirements

--- src/Client.php
+++ src/Client.php
@@ -25,7 +25,7 @@ class Client {
      * @const string Current version of this client.
      * This follows Semantic Versioning (http://semver.org/)
      */
-    const VERSION = '3.3.0';
+    const VERSION = '4.0.0';
 
     /**
      * @const string The API endpoint for Notify production.
